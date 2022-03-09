226da31414d8b46b6f99a6cf7ce974c5cb8f503a
kais-odoo <kais@odoo.com>
2022-03-10 00:26:59 +0530

    [IMP]website menu change

--- grievance/controllers/main.py
+++ grievance/controllers/main.py
@@ -5,7 +5,8 @@ from odoo.http import request
 class grievanceWebsite(http.Controller):
     @http.route('/grievance', website=True, auth='user', type="http")
     def grievance_form(self, **kw):
-        res_partner = request.env["res.partner"].search([])
+        id = request.env.user.partner_id.id
+        res_partner = request.env["res.partner"].search([('id','=',id)])
         grievance_type = request.env["grievance.type"].search([])
         grievance_department = request.env["department.register"].search([])
         return request.render("grievance.create_grievance_form_template",{
@@ -16,10 +17,17 @@ class grievanceWebsite(http.Controller):
 
 
 
-    @http.route('/create/grievance-registered', website=True, auth="user",type="http")
+    @http.route('/create/grievance-registered', website=True, auth="user",type="http", methods=['POST'])
     def grivance_form_register(self, **kw):
+        res_partner = request.env["res.partner"].search([('name','=',kw['Supporter'])])
+        grievance_type = request.env["grievance.type"].search([('name','=',kw['GrivanceType'])])
+        grievance_department = request.env["department.register"].search([('name','=',kw['Department'])])
+        request.env["grievance.register"].create({
+            'name' : kw['Subject'],
+            'description' : kw['Description'],
+            'supporters_ids' : res_partner,
+            'grievance_type_id' : grievance_type.id,
+            'department_id' : grievance_department.id
+        })
         
-        request.env["grievance.register"].create(kw)
-        request.env["grievance.type"].create(kw)
-        request.env["department.register"].create(kw)
         return request.render("grievance.successfully_register",{})
